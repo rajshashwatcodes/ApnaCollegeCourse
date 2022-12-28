f07aec60c25cb7868879d7e51d698355ae881ef7
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-12-28 20:12:57 +0530

    A program understand different variables in java.

---
+++ 1_Variables&DataTypes/variables.java
@@ -0,0 +1,17 @@
+public class variables {
+    public static void main(String args[]) {
+        // variables
+        String name = "Amisha Verma";
+        int a = 25;
+        int b = 10;
+        // double is used for decimals
+        double price = 25.16;
+        float pi = 3;
+
+        System.out.println(name);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(price);
+        System.out.print(pi);
+    }
+}
