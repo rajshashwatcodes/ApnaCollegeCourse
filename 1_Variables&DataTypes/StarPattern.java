a44889cff56554dce96a06c1985d9497077e2fd4
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-12-28 20:16:47 +0530

    A program to print a star pattern.

---
+++ 1_Variables&DataTypes/StarPattern.java
@@ -0,0 +1,13 @@
+// Print:
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+class StarPattern { //can alsobe written as "public class star_pattern"
+    public static void main(String[] args) {
+        // output
+        System.out.print("*\n* *\n* * *\n* * * *\n* * * * *");
+    }
+}
