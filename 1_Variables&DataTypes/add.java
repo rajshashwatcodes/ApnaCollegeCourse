7d31741c542410b8966cbe7966f030c6509f1031
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-12-28 20:11:03 +0530

    A program to find the sum of two numbers.

---
+++ 1_Variables&DataTypes/add.java
@@ -0,0 +1,9 @@
+public class add {
+    public static void main(String args[]) {
+        int a = 6;
+        int b = 12;
+        // System.out.print(a + b);
+        int sum = a + b;
+        System.out.print(sum);
+    }
+}
