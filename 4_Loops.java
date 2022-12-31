d5ec0b52887c4aa65edbb038953947a27c5e04a5
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2022-12-31 14:11:42 +0530

    A program using "For" loop.

---
+++ 4_Loops.java
@@ -0,0 +1,8 @@
+public class HelloLoop {
+    public static void main(String args[]) {
+        for(int i = 0; i < 5 ; i++) {
+            System.out.println("Hello");
+            i += 2;        
+        }
+    }
+}
