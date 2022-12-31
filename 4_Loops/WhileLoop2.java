ad5caa1a61eff7a17cf0e67c70ab68e8830c09fd
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2022-12-31 14:17:30 +0530

    A while loop program.

---
+++ 4_Loops/WhileLoop2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class WhileLoop2 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the last number: ");
+        int n = sc.nextInt();
+        int a = 1;
+        while(a <= n) {
+            System.out.print(a + " ");
+            a++;
+        }
+        sc.close();
+    }
+}
