02cce1b6b9ee2cc1e605a062f34a40b74007f364
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2022-12-31 14:13:45 +0530

    A for loop program.

---
+++ 4_Loops/ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop {
+    public static void main(String args[]) {
+        for(int i = 1 ; i <=10 ; i++) {
+            System.out.println("Hello World!");
+        }
+        System.out.println("'Hello world!' is printed 10 times.");
+    }
+}
