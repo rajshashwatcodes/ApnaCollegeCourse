446ebe94e4b3a98f9aba16068253f99b70e98670
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-01-03 23:21:25 +0530

    A program to make full pyramid of numbers.

---
+++ 6_AdvancePatterns(2)/NumberFullPyramid.java
@@ -0,0 +1,23 @@
+// To print the following pattern:
+//     1 
+//    2 2 
+//   3 3 3 
+//  4 4 4 4 
+// 5 5 5 5 5
+
+public class NumberFullPyramid {
+    public static void main(String args[]) {
+        int n = 5;
+        for(int i = 1; i <= n; i++) {
+            // spaces
+            for(int j = 1; j <= n-i ; j++) {
+                System.out.print(" ");
+            }
+            // number --> print row no. , row no. times
+            for(int k = 1; k <= i ; k++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
