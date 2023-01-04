39bdc8eae074fd669a00e88fcc726a8ecb1ba72e
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-01-04 23:59:16 +0530

    A factorial function.

---
+++ 7_Functions&Methods/Factorial.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Factorial {
+
+    public static void printFactorial(int n) {
+        // loop
+        if(n < 0 ) {
+            System.out.println("Invalid Number");
+            return;
+        }
+        int facto = 1;
+        for(int i = n; i >= 1; i--) {
+            facto = facto * i;
+        }
+
+        System.out.println("The factorial is: " + facto);
+        return;
+
+    }
+    public static void main(String args[]) {
+        System.out.print("Enter a number to find factorial: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        printFactorial(n);
+    }
+}
