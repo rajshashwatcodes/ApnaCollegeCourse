13038889d78e0a90fd43c7a94d1545a197b13802
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-01-06 23:55:43 +0530

    A program to make pairs of numbers in an array.

---
+++ 8_Arrays/ArrayPairs.java
@@ -0,0 +1,20 @@
+public class ArrayPairs {
+    public static void pairs(int numbers[]) {
+        int tp = 0; //total number of pairs
+        for(int i = 0 ; i < numbers.length ; i++){
+            // 2, 4, 6, 8, 10
+            int current = numbers[i];
+            for(int j = i + 1 ; j < numbers.length ; j++) {
+                System.out.print("(" + current + "," + numbers[j] + ")");
+                tp++;
+            }
+            System.out.println();
+        }
+        System.out.println("Total number of pairs are: " + tp);
+
+    }
+    public static void main(String args[]) {
+        int numbers[] = {2 , 4 , 6 , 8 , 10};
+        pairs(numbers);
+    }
+}
