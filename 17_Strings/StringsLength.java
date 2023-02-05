5b3197598f4c76830ddc144b1b40556ef671ab16
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-02-05 23:44:54 +0530

    A program to find string length.

---
+++ 17_Strings/StringsLength.java
@@ -0,0 +1,7 @@
+public class StringsLength {
+    public static void main(String args[]) {
+        String fullName = "Tony Stark"; //space will also be counted
+        System.out.println(fullName.length());
+        
+    }
+}
