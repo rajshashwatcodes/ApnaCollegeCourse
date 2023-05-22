71053e92ec224e1683017498b839cecbb80b16c0
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-05-22 23:22:33 +0530

    Update SelectionSorting.java

--- 9_Basic_Sorting_Algorithm/SelectionSorting.java
+++ 9_Basic_Sorting_Algorithm/SelectionSorting.java
@@ -4,7 +4,7 @@ public class SelectionSorting {
 
     public static void printArr(int arr[]) {
         for(int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i] + " ");
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
