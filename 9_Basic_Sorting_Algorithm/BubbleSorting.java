a237f87f66c95955f18d290764d08ecca0ae604b
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-05-22 23:21:48 +0530

    Update BubbleSorting.java

--- 9_Basic_Sorting_Algorithm/BubbleSorting.java
+++ 9_Basic_Sorting_Algorithm/BubbleSorting.java
@@ -17,7 +17,7 @@ public class BubbleSorting {
 
     public static void printArr(int arr[]) {
         for(int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i] + " ");
+            System.out.print(arr[i] + " ");
         }
 
         System.out.println();
