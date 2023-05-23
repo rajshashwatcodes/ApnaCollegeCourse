311aa72e93efc20cf09a093216de8b30f7272a42
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-05-23 23:22:47 +0530

    Update BubbleSorting.java

--- 9_Basic_Sorting_Algorithm/BubbleSorting.java
+++ 9_Basic_Sorting_Algorithm/BubbleSorting.java
@@ -3,16 +3,19 @@ package I_Basic_Sorting_Algorithm;
 public class BubbleSorting {
 
     public static void bubbleSortFunc(int arr[]) {
-        for(int turn = 0; turn < arr.length - 1; turn++) {
+        int swaps = 0;
+        for(int turn = 0; turn < arr.length - 1; turn++) {  
             for(int j = 0; j < arr.length - 1 - turn; j++) {
                 if(arr[j] > arr[j + 1]) {
                     // swap // defining a temporary value
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp; 
-                }
-            }
-        } 
+                    swaps++;
+                }    
+            }  
+        }
+        System.out.println(" Swaps: " + swaps); 
     }
 
     public static void printArr(int arr[]) {
@@ -25,6 +28,7 @@ public class BubbleSorting {
     public static void main(String[] args) {
         int arr[] = {5 , 4 , 1 , 3 , 2};
         bubbleSortFunc(arr);
+        System.out.print("Sorted array: ");
         printArr(arr);
     }
 }
