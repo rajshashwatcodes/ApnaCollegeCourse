1288534ca87bc588ba7f91f3cddfae461776ac28
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-10-16 23:43:05 +0530

    Delete 17_Strings directory

--- 17_Strings/StringConcatenation.java
+++
@@ -1,8 +0,0 @@
-public class StringConcatenation {
-    public static void main(String args[]) {
-        String firstName = "Shashwat";
-        String lastName = "Raj";
-        String fullName = firstName + " " + lastName;
-        System.out.println(fullName);
-    }
-}
--- 17_Strings/StringsLength.java
+++
@@ -1,7 +0,0 @@
-public class StringsLength {
-    public static void main(String args[]) {
-        String fullName = "Tony Stark"; //space will also be counted
-        System.out.println(fullName.length());
-        
-    }
-}
